9f4873d3edc43fb4f04d4adb5effb0f5ae8ccbe7
Sagnik Bhattacharya <sagnikdps@gmail.com>
2021-11-02 09:00:27 -0700

    hw4 q2, q3 maybe done

--- hw4/cs285/scripts/run_q3.sh
+++ hw4/cs285/scripts/run_q3.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-python cs285/scripts/run_hw4_mb.py --exp_name q3_obstacles \
+python cs285/scripts/run_hw4_mb.py --exp_name q3_obstacles2 \
     --env_name obstacles-cs285-v0 --add_sl_noise \
-    --num_agent_train_steps_per_iter 20 --batch_size_initial 5000 \
+    --num_agent_train_steps_per_iter 50 --batch_size_initial 5000 \
     --batch_size 1000 --mpc_horizon 10 \
-    --n_iter 12 --mpc_action_sampling_strategy 'random'
+    --n_iter 30 --mpc_action_sampling_strategy 'random'
 
-python cs285/scripts/run_hw4_mb.py --exp_name q3_reacher \
+python cs285/scripts/run_hw4_mb.py --exp_name q3_reacher2 \
     --env_name reacher-cs285-v0 --add_sl_noise --mpc_horizon 10 \
     --num_agent_train_steps_per_iter 1000 --batch_size_initial 5000 \
-    --batch_size 5000 --n_iter 15 --mpc_action_sampling_strategy 'random'
+    --batch_size 5000 --n_iter 50 --mpc_action_sampling_strategy 'random'
 
-python cs285/scripts/run_hw4_mb.py --exp_name q3_cheetah --env_name \
-    cheetah-cs285-v0 --mpc_horizon 15 --add_sl_noise \
+python cs285/scripts/run_hw4_mb.py --exp_name q3_cheetah2 \
+    --env_name cheetah-cs285-v0 --mpc_horizon 15 --add_sl_noise \
     --num_agent_train_steps_per_iter 1500 --batch_size_initial 5000 \
-    --batch_size 5000 --n_iter 20 --mpc_action_sampling_strategy 'random'
+    --batch_size 5000 --n_iter 60 --mpc_action_sampling_strategy 'random'
