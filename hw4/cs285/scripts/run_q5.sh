e2d948dc693cad14d24d9aa09c1b65708c94ffcf
Sagnik Bhattacharya <sagnikdps@gmail.com>
2021-11-05 16:56:39 -0700

    Some q5

---
+++ hw4/cs285/scripts/run_q5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+python cs285/scripts/run_hw4_mb.py --exp_name q5_cheetah_random \
+    --env_name 'cheetah-cs285-v0' --mpc_horizon 15 --add_sl_noise \
+    --num_agent_train_steps_per_iter 1500 --batch_size_initial 5000 \
+    --batch_size 5000 --n_iter 5 --mpc_action_sampling_strategy 'random'
+
+python cs285/scripts/run_hw4_mb.py --exp_name q5_cheetah_cem_2 \
+    --env_name 'cheetah-cs285-v0' --mpc_horizon 15 --add_sl_noise \
+    --num_agent_train_steps_per_iter 1500 --batch_size_initial 5000 \
+    --batch_size 5000 --n_iter 5 --mpc_action_sampling_strategy 'cem' \
+    --cem_iterations 2
+
+python cs285/scripts/run_hw4_mb.py --exp_name q5_cheetah_cem_4 \
+    --env_name 'cheetah-cs285-v0' --mpc_horizon 15 --add_sl_noise \
+    --num_agent_train_steps_per_iter 1500 --batch_size_initial 5000 \
+    --batch_size 5000 --n_iter 5 --mpc_action_sampling_strategy 'cem' \
+    --cem_iterations 4
