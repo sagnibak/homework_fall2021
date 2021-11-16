f59e716a83250fab30be20116e401ae56290a9c4
Sagnik Bhattacharya <sagnikdps@gmail.com>
2021-11-15 18:59:27 -0800

    hw5 q2 p1 hint

--- hw5/cs285/scripts/run_q2_p1_hint.sh
+++ hw5/cs285/scripts/run_q2_p1_hint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 python cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 \
-    --exp_name q2_dqn_shift2_scale100 \
+    --exp_name q2_dqn_shift-1_scale10 \
     --use_rnd --unsupervised_exploration --offline_exploitation --cql_alpha=0 \
-    --exploit_rew_shift 2 --exploit_rew_scale 100
+    --exploit_rew_shift -1 --exploit_rew_scale 10
 
 python cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 \
-    --exp_name q2_cql_shift2_scale100 \
+    --exp_name q2_cql_shift-1_scale10 \
     --use_rnd --unsupervised_exploration --offline_exploitation --cql_alpha=0.1 \
-    --exploit_rew_shift 2 --exploit_rew_scale 100
+    --exploit_rew_shift -1 --exploit_rew_scale 10
